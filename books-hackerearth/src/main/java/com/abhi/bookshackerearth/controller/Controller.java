53e581d878df866c4d5f3c07bd0693ba827afe20
Abhinav Srivastava <abhinavsrivastava54@yahoo.com>
2021-09-24 11:46:12 +0530

    Implement FuzzySearch
    Implemented fuzzy search for title of the book

--- books-hackerearth/src/main/java/com/abhi/bookshackerearth/controller/Controller.java
+++ books-hackerearth/src/main/java/com/abhi/bookshackerearth/controller/Controller.java
@@ -3,34 +3,67 @@ package com.abhi.bookshackerearth.controller;
 import com.abhi.bookshackerearth.dao.Dao;
 import com.abhi.bookshackerearth.dao.Repository;
 import com.abhi.bookshackerearth.entity.Book;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 //@RestController
 //@org.springframework.stereotype.Controller
 
-@org.springframework.stereotype.Controller
+//@org.springframework.stereotype.Controller
 @RequestMapping("/api")
+@RestController //since this is rest controller and jackson is added in dependency, the output on web is JSON format
 public class Controller {
 
-    //declare dependencies of Dao objects which are actually
+    //declare dependencies of Dao objects which are actually accessing the database
     @Autowired
     private Dao dao;
     @Autowired
     Repository repository;
+
+    //declare and define listeners so that output can be received from server
     @GetMapping("/api/{bookID}")
+    //path variable required to receive data from the database
         public Book bookPage(@PathVariable int bookID) {
-        System.out.println("yes you got in");
+        System.out.println("fetching your book");
+        //Optional is returned by the default methods available in CrudRepository interface
         Optional<Book> optional = repository.findById(bookID);
-        if(optional.isPresent()) {
-            System.out.println(optional.get());
+        if(optional.isPresent()) { //if any entry is found then run below code
+            System.out.println(optional.get()); //get() returns the value if its present which in this case should be
+            //the book object
             return optional.get();
         } else {
             System.out.println("There is an error");
+        }
+        return null;
+    }
+
+    //declare and define fuzzy search listeners so that output can be received from server
+    @GetMapping("/api/title")
+    //using path variable can be a little confusing as springboot controller thinks the above getId program can be called as well
+    //by using requestparam we have segregated the two calls for the dispatcher
+    public String searchBookByTitleFuzzy(@RequestParam(value="title") String title) {
+        System.out.println("fetching your books");
+        //since we are returning list,we save the result in a list
+        List<Book> optional = dao.fuzzySearch(title);
+        //fuzzysearch functioning is provided in the dao class
+        if(optional.isEmpty()) { //if no entry is found, we run below code
+            System.out.println("nothing to return here");
+            return null;
+        } else {
+            //mapper allows us to convert the List to a JSON array
+            //try catch block is used to check for any exceptions
+            //also its mandatory to either use a catch block or throw in method declaration
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                return mapper.writeValueAsString(optional);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
         }
         return null;
     }
--- books-hackerearth/src/main/java/com/abhi/bookshackerearth/dao/Dao.java
+++ books-hackerearth/src/main/java/com/abhi/bookshackerearth/dao/Dao.java
@@ -8,16 +8,26 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Query;
+import java.util.List;
 
 @Component
 @Repository
 public class Dao {
 
-    SessionFactory factory;
+    SessionFactory factory; //its a factory of session (that is creates and provides session object
+    //it provides factory method to get the object of session
+    //its a client of ClientProvider
+    //holds second level cache of data (optional)
+//    ConnectionProvider is a factory of JDBC connections. It abstracts the application from DriverManager or DataSource.
     Session session;
+    //it provides an interface between application and data stored in database
+    //its short lived
+    //it wraps the JDBC connection
+    //its a factory of Transcation, Query and Criteria
+    //holds first level cache of data (mandatory)
+    //org.hibernate.Session provides methods to insert, update and delete objects.
     public Dao() {
-         factory = new Configuration().addAnnotatedClass(Book.class).configure("hibernate.cfg.xml").buildSessionFactory();
-
+        factory = new Configuration().addAnnotatedClass(Book.class).configure("hibernate.cfg.xml").buildSessionFactory();
         session = factory.getCurrentSession();
     }
 
@@ -26,11 +36,8 @@ public class Dao {
         factory = new Configuration().addAnnotatedClass(Book.class).configure("hibernate.cfg.xml").buildSessionFactory();
 
         session = factory.getCurrentSession();
-//        SessionFactory factory = new Configuration().addAnnotatedClass(com.abhi.entity.Book.class).configure("hibernate.cfg.xml").buildSessionFactory();
-//
-//        Session session = factory.getCurrentSession();
-        System.out.println("Is FACTORY OPEN????" + factory.isOpen());
-        System.out.println("Is SESSION OPEN????" + session.isOpen());
+        System.out.println("Is FACTORY OPEN? : " + factory.isOpen());
+        System.out.println("Is SESSION OPEN? : " + session.isOpen());
         try {
             session.beginTransaction();
             session.save(objectBook);
@@ -58,5 +65,25 @@ public class Dao {
             factory.close();
         }
         return null;
+    }
+
+    // this Dao method allows us to fuzzy search for the title of the book.
+    //kind of like google
+    public List fuzzySearch(String searchString) {
+        factory = new Configuration().addAnnotatedClass(Book.class).configure("hibernate.cfg.xml").buildSessionFactory();
+        session = factory.getCurrentSession();
+        try {
+            session.beginTransaction();
+            //after session is started, we run below query to check for the title
+            Query qp = session.createQuery(" from book where title like ?0").setParameter(0, "%"+searchString+"%");
+            System.err.println(qp.toString() + "IS THE QUERY STRING USED");
+            return qp.getResultList(); //returns the result set
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            factory.close();
+        }
+        return null; //null if anothing worked above
     }
 }
